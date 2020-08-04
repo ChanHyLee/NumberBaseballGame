349e409c261d01baad0094a7986e7f54d46f5ca3
ChanHyLee <65821042+ChanHyLee@users.noreply.github.com>
2020-08-04 15:44:08 +0900

    Update start.js

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 const openingWrap = document.querySelector(".js-openingWrap");
 const gameWrap = document.querySelector(".js-gameWrap");
+const submitWrap = document.querySelector(".js-submitWrap");
 const startBtn = document.querySelector(".js-startBtn");
 
 const SHOWING_CLASS = "showing";
@@ -7,6 +8,7 @@ const SHOWING_CLASS = "showing";
 function handleStartBtn(event) {
     openingWrap.classList.remove(SHOWING_CLASS);
     gameWrap.classList.add(SHOWING_CLASS);
+    submitWrap.classList.add(SHOWING_CLASS);
 }
 
 function init() {
