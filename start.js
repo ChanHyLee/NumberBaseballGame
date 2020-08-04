7268b07b46b73c90fe644f619d2db938ac2dfce7
ChanHyLee <65821042+ChanHyLee@users.noreply.github.com>
2020-08-05 00:25:16 +0900

    Update start.js

--- start.js
+++ start.js
@@ -2,18 +2,27 @@ const openingWrap = document.querySelector(".js-openingWrap");
 const gameWrap = document.querySelector(".js-gameWrap");
 const submitWrap = document.querySelector(".js-submitWrap");
 const startBtn = document.querySelector(".js-startBtn");
+const dummyInput = document.querySelector(".js-dummyInput");
 
 const SHOWING_CLASS = "showing";
 
 function handleStartBtn(event) {
     openingWrap.classList.remove(SHOWING_CLASS);
     gameWrap.classList.add(SHOWING_CLASS);
+    dummyInput.blur();
     submitWrap.classList.add(SHOWING_CLASS);
+    numInput.focus();
 }
 
 function init() {
     openingWrap.classList.add(SHOWING_CLASS);
     gameWrap.classList.remove(SHOWING_CLASS);
     startBtn.addEventListener("click", handleStartBtn);
+    dummyInput.focus();
+    dummyInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            startBtn.click();
+        }
+    });
 }
 init();
